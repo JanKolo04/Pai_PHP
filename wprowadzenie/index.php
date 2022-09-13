2af8a02dc1603c137f951d7cb0b77988d75f32ad
JanKolo04 <jankolodziej04@gmail.com>
2022-09-13 11:38:05 +0200

    First lesson

---
+++ wprowadzenie/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Wrpowadzenie</title>
+</head>
+<body>
+
+    <?php
+    
+        function main() {
+            $counter = 1;
+
+            echo "Hello world</br>";
+            echo "My counter",$counter."</br>";
+            echo "Hello friend</br>";
+        }
+
+        main();
+    
+    ?>
+
+</body>
+</html>
